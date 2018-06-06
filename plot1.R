71733471079439ba5b716fc8f42c2ddbb9629962
R R <rr62@live.com>
2018-06-06 15:31:37 -0400

    update1

--- plot1.R
+++ plot1.R
@@ -14,6 +14,6 @@ df <- df[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 png("plot1.png", width=480, height=480)
 
 hist(df[, Global_active_power], main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Blue")
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 dev.off()
